8d81b19dc5cc0fa352c0f7e698c5c84e396a69d2
GunJJu <smkim0721@gmail.com>
2025-08-20 16:14:01 +0900

    .

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './DiaryItem.css'
 import Button from './Button'
 import { getEmoticonImg } from '../util/getEmoticonImg'
 
 const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+    const nav = useNavigate()
 
     return (
         <div className='DiaryItem'>
-            <div className={`img-section bg-${emotionId}`}>
+            <div className={`img-section bg-${emotionId}`}
+                onClick={() => nav(`/diary/${id}`)}>
                 <img src={getEmoticonImg(emotionId)} alt="icon" />
             </div>
-            <div className="info-section">
+            <div className="info-section"
+                onClick={() => nav(`/diary/${id}`)}>
                 <div className="created-date">{new Date(createdDate).toLocaleDateString()}</div>
                 <div className="content">{content}</div>
             </div>
             <div className="button-section">
-                <Button text={"수정하기"} />
+                <Button text={"수정하기"} onClick={() => nav(`/edit/${id}`)} />
             </div>
         </div>
     )
--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import DiaryItem from './DiaryItem'
 import Button from './Button'
 import './DiaryList.css'
+import { useNavigate } from 'react-router-dom'
 
 const DiaryList = ({ data }) => {
+    const nav = useNavigate()
     return (
         <div className='DiaryList'>
             <div className="menu-bar">
@@ -11,7 +13,7 @@ const DiaryList = ({ data }) => {
                     <option value="latest">최신순</option>
                     <option value="oldest">오래된순</option>
                 </select>
-                <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+                <Button text={"새 일기 쓰기"} type={"POSITIVE"} onClick={() => nav('/new')} />
             </div>
             <div className="list-wrapper">
                 {data.map((item) => (
